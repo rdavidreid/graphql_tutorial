0ac087de86a6a1cd3df85f9e3e85d249e58d2539
David <r.david.reid@gmail.com>
2022-03-17 16:10:38 -0700

    React App Setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+// components
+import BookList from './components/BookList';
+
+
+function App() {
+  return (
+    <div id='main'>
+      <h1>GraphQL Reading List</h1>
+      <BookList />
+
+    </div>
+  );
+}
+
+export default App;
